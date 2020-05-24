69b7b64e045b46276f4aa6b641f5c8af33bfb04f
Victor <avakado58rus@gmail.com>
2020-05-24 18:26:20 +0300

    Зафиксирован размер главного окна

--- KT_PO_lab/Form1.Designer.cs
+++ KT_PO_lab/Form1.Designer.cs
@@ -182,6 +182,8 @@
             this.Controls.Add(this.butGenerate);
             this.Font = new System.Drawing.Font("Jokerman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximumSize = new System.Drawing.Size(539, 746);
+            this.MinimumSize = new System.Drawing.Size(539, 746);
             this.Name = "MainForm";
             this.Text = "Качество и тестирование  ПО";
             this.ResumeLayout(false);
